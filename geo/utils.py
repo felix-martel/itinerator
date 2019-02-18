d2f1624bc332f1b67e2a275c62ab4512fa373edb
felix <felix.martel@polytechnique.edu>
2019-02-18 17:14:16 -0500

    Add function to compute climb and descent data from a track

--- geo/utils.py
+++ geo/utils.py
@@ -15,3 +15,16 @@ def convert(*args, from_unit=None, to_unit=None):
         "km": 1000
     }
     return tuple([arg * (units[from_unit] / units[to_unit]) for arg in args])
+
+def denivele(elevations):
+    dpos = 0
+    dneg = 0
+    i = 0
+    for j in range(1, len(elevations)):
+        delta = elevations[j] - elevations[i]
+        i = j
+        if delta > 0:
+            dpos += delta
+        else:
+            dneg -= delta
+    return dpos, dneg
