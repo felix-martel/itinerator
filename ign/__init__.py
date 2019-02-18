7db79730082854e24c46b219086fd2b114488d16
felix <felix.martel@polytechnique.edu>
2019-02-18 17:15:24 -0500

    Update IGN client with more query capabilities

--- ign/__init__.py
+++ ign/__init__.py
@@ -1,13 +1,16 @@
 from .zoom import zoom as levels
 from .mset import DEFAULT as __default_mset
+import ign.layers as layers
 from owslib.wmts import WebMapTileService
-
+import ign.api as api
+import ign.zoom as zoom
 TILE_SIZE = 256
 API = "https://wxs.ign.fr/choisirgeoportail/geoportail/wmts"
+ALTI_API = "https://wxs.ign.fr/choisirgeoportail/alti/rest/"
 
 wmts = WebMapTileService(API)
 
-_query = "&".join([
+tile_query = "&".join([
     "SERVICE=WMTS",
     "REQUEST=GetTile",
     "VERSION=1.0.0",
@@ -20,11 +23,16 @@ _query = "&".join([
     "FORMAT=image/jpeg"
 ])
 
+alti_query = "elevation.json?lon={lon}&lat={lat}&zonly=true"
+
 def query(layer, level, col, row):
-    return _query.format(
+    return tile_query.format(
         mset=__default_mset,
         layer=layer,
         level=level,
         col=col,
         row=row
     )
+
+def alti(lon, lat):
+    return ALTI_API + alti_query.format(lon=lon, lat=lat)
---
+++ ign/api.py
@@ -0,0 +1,89 @@
+from owslib.wmts import WebMapTileService
+import ign.mset
+import requests
+import json
+import io
+from math import floor
+import pyproj
+from .zoom import zoom
+from PIL import Image
+
+
+API = "https://wxs.ign.fr/choisirgeoportail/geoportail/wmts?"
+ALTI_API = "https://wxs.ign.fr/choisirgeoportail/alti/rest/"
+
+wmts = WebMapTileService(API)
+
+tile_query = "&".join([
+    "SERVICE=WMTS",
+    "REQUEST=GetTile",
+    "VERSION=1.0.0",
+    "&LAYER={layer}",
+    "TILEMATRIXSET={mset}",
+    "TILEMATRIX={level}",
+    "TILECOL={col}",
+    "TILEROW={row}",
+    "STYLE=normal",
+    "FORMAT=image/jpeg"
+])
+
+
+def query(layer, level, col, row):
+    return tile_query.format(
+        mset=ign.mset.DEFAULT,
+        layer=layer,
+        level=level,
+        col=col,
+        row=row
+    )
+
+def tile_matrix(level):
+    return wmts.tilematrixsets[ign.mset.DEFAULT].tilematrix[str(level)]
+
+def alti_query(lon, lat):
+    return ALTI_API + "elevation.json?lon={lon}&lat={lat}&zonly=true".format(lon=lon, lat=lat)
+
+def get_elevation(lon, lat):
+    encode = lambda l: "|".join([str(v) for v in l])
+    r = requests.get(alti_query(encode(lon), encode(lat))).content
+    el = json.load(io.BytesIO(r))["elevations"]
+    return el
+
+def tile_attributes(level):
+    matrix = tile_matrix(level)
+    tile_size = matrix.tileheight * zoom[level].res  # taille d'une tuile en mètres
+    x0, y0 = matrix.topleftcorner
+    return tile_size, (x0, y0)
+
+def get_coords(p, level):
+    tile_size, (x0, y0) = tile_attributes(level)
+
+    # Convert coordinates to Mercator
+    x, y = p.as_mercator()
+
+    # Get tile column & row
+    col = floor((x - x0) / tile_size)
+    row = floor((y0 - y) / tile_size)
+
+    return col, row
+
+def reverse_coords(col, row, level):
+    tile_size, (x0, y0) = tile_attributes(level)
+
+    x = x0 + (col * tile_size)
+    y = y0 - (row * tile_size)
+
+    return x, y
+
+def get_tile(col, row, layer, level):
+    url = API + query(layer=layer, level=level, col=col, row=row)
+    response = requests.get(url)
+    try:
+        bin_im = io.BytesIO(response.content)
+        return Image.open(bin_im)
+    except OSError as e:
+        print(col, row, layer, level)
+        print(url)
+        print(response.text)
+        raise e
+
